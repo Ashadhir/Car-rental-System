50b19fc22dfdbb5c6f269d73921d52c91b705494
Tharaga <Tharaga@Tharaga-PC>
2016-03-12 15:46:48 +0530

    mathangi commit

--- src/first.java
+++ src/first.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Ahamed Shadhir
- */
 public class first {
     public static void main(String[] args) {
         System.out.println("hello guyz");
         System.out.println("vaanga seiyalaam");
         System.out.println("nalla seiyanum");
         System.out.println("I'm the leader of this project"); //madhu
+        System.out.println("gfdytyg yg");
     }
 }
